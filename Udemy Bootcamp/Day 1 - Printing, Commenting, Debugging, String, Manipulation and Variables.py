e89e331bd9e22dbded18dcf75b267d7015bc606a
Taekyu-Na <factualist623@gmail.com>
2025-09-04 23:41:20 +0900

    Day 1 - Printing, Commenting, Debugging, String, Manipulation and Variables.py

--- Udemy Bootcamp/Day 1 - Printing, Commenting, Debugging, String, Manipulation and Variables.py
+++ Udemy Bootcamp/Day 1 - Printing, Commenting, Debugging, String, Manipulation and Variables.py
@@ -1,1 +1,42 @@
+# print() function
+
 print("Hello World!")
+print("Hello World!\nHello World!\nHello World!")
+print("Hello " + "TK")
+print("Hello" + " TK")
+print("Hello" + " " + "TK")
+
+#Debugging Test 
+print(Notes from Day 1")
+ print("The print statement is used to output strings")
+print("Strings are strings of characters"
+priint("String Concatenation is done with the + sign")
+print(("New lines can be created with a \ and the letter n")
+
+print("Notes from Day 1")
+print("The print statement is used to output strings")
+print("Strings are strings of characters")
+print("String Concatenation is done with the + sign")
+print("New lines can be created with a \ and the letter n")
+
+print("Notes " + "from " + "Day " + "1")
+print("The " + "print " + "statement " + "is " + "used " + "to " + "output " + "strings")
+print("Strings " + "are " + "strings " + "of " + "characters")
+print("String " + "Concatenation " + "is " + "done " + "with " + "the " + "+ " + "sign")
+print("New " + "lines " + "can " + "be " + "created " + "with " + "a " + "\ " + "and " + "the " + "letter " + "n")
+
+# input() function- 유저가 입력한 값으로 대체됨
+input("What is your name?")
+print("Hello " + input("What is your name?"))
+
+
+
+
+
+
+
+
+
+
+
+      
