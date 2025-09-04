60d4d94f3198d63b89cf820f11624351a71e1719
Taekyu-Na <factualist623@gmail.com>
2025-09-05 00:28:07 +0900

    Day 1 - Printing, Commenting, Debugging, String, Manipulation and Variables.py

--- Udemy Bootcamp/Day 1 - Printing, Commenting, Debugging, String, Manipulation and Variables.py
+++ Udemy Bootcamp/Day 1 - Printing, Commenting, Debugging, String, Manipulation and Variables.py
@@ -6,7 +6,7 @@ print("Hello " + "TK")
 print("Hello" + " TK")
 print("Hello" + " " + "TK")
 
-//Debugging Test 
+//Debugging Test
 # print(Notes from Day 1")
 #  print("The print statement is used to output strings")
 # print("Strings are strings of characters"
@@ -43,6 +43,34 @@ print(name)
 
 #len() function - Character Length 구하기 
 print(len(input("What is your name?")))
+
+//Debugging Test
+# 우리는 glass1과 glass2라는 2개의 변수를 가지고 있습니다.
+# glass1에는 우유가 담겨 있고, glasss에는 주스가 들어 있습니다.
+# 변수의 내용을 교환하는 3줄의 코드를 작성하십시오.
+# "milk"나 "juice"라는 단어를 입력해서는 안 됩니다.
+# 이 연습을 해결하기 위해 변수만 사용할 수 있습니다.
+glass1 = "milk"
+glass2 = "juice"
+
+temp = glass1
+glass1 = glass2
+glass2 = temp
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
