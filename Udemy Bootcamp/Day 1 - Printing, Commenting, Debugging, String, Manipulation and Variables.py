410c44be6e1e31e6793ff98ad578dc6c33c1b490
Taekyu-Na <factualist623@gmail.com>
2025-09-10 22:45:10 +0900

    Day 1 - Printing, Commenting, Debugging, String, Manipulation and Variables.py

--- Udemy Bootcamp/Day 1 - Printing, Commenting, Debugging, String, Manipulation and Variables.py
+++ Udemy Bootcamp/Day 1 - Printing, Commenting, Debugging, String, Manipulation and Variables.py
@@ -1,4 +1,4 @@
-# print() function
+print function
 
 print("Hello World!")
 print("Hello World!\nHello World!\nHello World!")
@@ -6,7 +6,7 @@ print("Hello " + "TK")
 print("Hello" + " TK")
 print("Hello" + " " + "TK")
 
-//Debugging Test
+Debugging Test
 # print(Notes from Day 1")
 #  print("The print statement is used to output strings")
 # print("Strings are strings of characters"
@@ -25,11 +25,11 @@ print("Strings " + "are " + "strings " + "of " + "characters")
 print("String " + "Concatenation " + "is " + "done " + "with " + "the " + "+ " + "sign")
 print("New " + "lines " + "can " + "be " + "created " + "with " + "a " + "\ " + "and " + "the " + "letter " + "n")
 
-# input() function- 유저가 입력한 값으로 대체됨
+input function- 유저가 입력한 값으로 대체됨
 input("What is your name?")
 print("Hello " + input("What is your name?"))
 
-# Python Variable
+Python Variable
 name = input("What is your name?")
 print(name)
 
@@ -41,10 +41,10 @@ print(name)
 name = "TK"
 print(name)
 
-#len() function - Character Length 구하기 
+len function - Character Length 구하기 
 print(len(input("What is your name?")))
 
-//Debugging Test
+Debugging Test
 # 우리는 glass1과 glass2라는 2개의 변수를 가지고 있습니다.
 # glass1에는 우유가 담겨 있고, glasss에는 주스가 들어 있습니다.
 # 변수의 내용을 교환하는 3줄의 코드를 작성하십시오.
@@ -56,6 +56,8 @@ glass2 = "juice"
 temp = glass1
 glass1 = glass2
 glass2 = temp
+
+Project
 
 var1 = input("Welcome to the Band Name Generator.\nWhat's the name of the city you grew up in?\n")
 var2 = input("What's your pet's name?\n")
