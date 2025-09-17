34f8c833a4d1b1e8e3f16e35f4e369fd33711588
Taekyu-Na <factualist623@gmail.com>
2025-09-17 21:03:38 +0900

    Day 3 - Conditional Statements, Logical Operators, Code Blocks, and Scope.py

--- Udemy Bootcamp/Day 3 - Conditional Statements, Logical Operators, Code Blocks, and Scope.py
+++ Udemy Bootcamp/Day 3 - Conditional Statements, Logical Operators, Code Blocks, and Scope.py
@@ -1,1 +1,20 @@
-Conditional if / else
+Conditional, if / else
+
+if condition:
+  do this
+else:
+  do this
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+Codeblock
+if height > 120:                                 #Codeblock1
+    print("You can ride the rollercoaster.")     #Codeblock1
+else:                                              #Codeblock2
+    print("You cannot ride the rollercoaster.")    #Codeblock2
+
+Comparison Operation
+>, <, >=, <=, ==, !=
+# "=" 1개는 변수 할당
+# "==" 2개는 값 일치 확인
