daddf0ff9922a3e638b548b5e049693563970a60
Taekyu-Na <factualist623@gmail.com>
2025-09-17 23:33:40 +0900

    Day 3 - Conditional Statements, Logical Operators, Code Blocks, and Scope.py

--- Udemy Bootcamp/Day 3 - Conditional Statements, Logical Operators, Code Blocks, and Scope.py
+++ Udemy Bootcamp/Day 3 - Conditional Statements, Logical Operators, Code Blocks, and Scope.py
@@ -196,7 +196,7 @@ var1 = input('You\'re at a cross road. Where do you want to go?\nType "left" or 
 
 if var1.lower() == "left":
     print("You've come to a lake. There is an island in the middle of the lake.")
-    var2 = input("Type \"wait\" to wait for a boat. Type \"swim\" to swim across.\n")
+    var2 = input('Type "wait" to wait for a boat. Type "swim" to swim across.\n')
     if var2.lower() == "wait":
         print("You arrive at the island unharmed. There is a house with 3 doors.")
         var3 = input("One red, one yellow and one blue. Which colour do you choose?\n")
@@ -210,4 +210,35 @@ if var1.lower() == "left":
         print("You get attacked by an angry trout. Game Over.")
 else:
     print("You fell into a hole. Game Over.")
+
+# 아래도 가능
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+var1 = input('You\'re at a cross road. Where do you want to go?\nType "left" or "right"\n').lower()
+
+if var1 == "left":
+    var2 = input('You\'ve come to a lake. There is an island in the middle of the lake."\n'
+                 'Type "wait" to wait for a boat. Type "swim" to swim across.\n').lower()
+    if var2 == "wait":
+        var3 = input('You arrive at the island unharmed. There is a house with 3 doors.\n'
+                     'One red, one yellow and one blue. Which colour do you choose?\n').lower()
+        if var3 == "red":
+            print("It's a room full of fire. Game Over.")
+        elif var3 == "blue":
+            print("You enter a room of beasts. Game Over")
+        elif var3 == "yellow":
+            print("You found the treasure! You Win!")
+        else:
+            print("You chose a door that doesn't exist. Game Over.")
+    else:
+        print("You get attacked by an angry trout. Game Over.")
+else:
+    print("You fell into a hole. Game Over.")
+
+
+
+
+
+
+
 
