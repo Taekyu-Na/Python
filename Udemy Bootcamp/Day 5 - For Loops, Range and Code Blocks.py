3d2d9626e2363827fbe2387c7e73bf4d8d6e2626
Taekyu-Na <factualist623@gmail.com>
2025-09-23 01:50:44 +0900

    Day 5 - For Loops, Range and Code Blocks.py

--- Udemy Bootcamp/Day 5 - For Loops, Range and Code Blocks.py
+++ Udemy Bootcamp/Day 5 - For Loops, Range and Code Blocks.py
@@ -46,8 +46,18 @@ for number in range(1, 101):
     else:
         print(number)
 
+
+
 Project
-Easy Version
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 import random
 
@@ -72,10 +82,12 @@ for number in numbers:
     elif len(var3) < nr_numbers:
         var3 = var3 + random.choice(numbers)
 
-print(f"Your password is: {var1}{var2}{var3}")
+password = var1+var2+var3
+print(password)
 
-Hard Version
+password2 = list(password)
+random.shuffle(password2)
+final = "".join(password2)
+print(final)
 
-
-
-
+print(f"Your password is: {final}") #또는 password 변수
