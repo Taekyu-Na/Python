e6a29d2f64d5b2aac83199fed69347caae6e9fee
Taekyu-Na <factualist623@gmail.com>
2025-09-23 01:03:52 +0900

    Day 5 - For Loops, Range and Code Blocks.py

--- Udemy Bootcamp/Day 5 - For Loops, Range and Code Blocks.py
+++ Udemy Bootcamp/Day 5 - For Loops, Range and Code Blocks.py
@@ -46,3 +46,36 @@ for number in range(1, 101):
     else:
         print(number)
 
+Project
+Easy Version
+
+import random
+
+var1 = ''
+for letter in letters:
+    if len(var1) == nr_letters:
+        var1 = var1
+    elif len(var1) < nr_letters:
+        var1 = var1 + random.choice(letters)
+
+var2 = ''
+for symbol in symbols:
+    if len(var2) == nr_symbols:
+        var2 = var2
+    elif len(var2) < nr_symbols:
+        var2 = var2 + random.choice(symbols)
+
+var3 = ''
+for number in numbers:
+    if len(var3) == nr_numbers:
+        var3 = var3
+    elif len(var3) < nr_numbers:
+        var3 = var3 + random.choice(numbers)
+
+print(f"Your password is: {var1}{var2}{var3}")
+
+Hard Version
+
+
+
+
