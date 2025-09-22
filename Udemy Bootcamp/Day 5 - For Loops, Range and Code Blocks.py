35faf68a547e9808fa8fa8aad0c7e2ff84727ed3
Taekyu-Na <factualist623@gmail.com>
2025-09-22 23:26:07 +0900

    Day 5 - For Loops, Range and Code Blocks.py

--- Udemy Bootcamp/Day 5 - For Loops, Range and Code Blocks.py
+++ Udemy Bootcamp/Day 5 - For Loops, Range and Code Blocks.py
@@ -36,3 +36,13 @@ for number in range(1, 101):
 print(total)
 
 FizzBuzz
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("Fizzbuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
+
