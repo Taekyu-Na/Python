07175eb05ed801ef93f2864e8ffb063dda699a52
Taekyu-Na <factualist623@gmail.com>
2025-12-02 21:43:43 +0900

    Day 6 - Functions, Code Blocks, and While Loops.py

--- Udemy Bootcamp/Day 6 - Functions, Code Blocks, and While Loops.py
+++ Udemy Bootcamp/Day 6 - Functions, Code Blocks, and While Loops.py
@@ -26,3 +26,25 @@ turn_right()
 move()
 turn_right()
 move()
+
+#World Hurdles Challenge
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+for number in range(1, 7):
+    jump()
+여기서 for number in range(6):
+    jump() 도 가능
+
