d6b5d68dc1797e32e49fc79031d9b5a912bbacc8
Taekyu-Na <factualist623@gmail.com>
2025-12-02 21:13:36 +0900

    Day 6 - Functions, Code Blocks, and While Loops to Day 6 - Functions, Code Blocks, and While Loops.py

---
+++ Udemy Bootcamp/Day 6 - Functions, Code Blocks, and While Loops.py
@@ -0,0 +1,18 @@
+Functions
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+my_function()
+
+Reeborg's World
+
+def turn_around():
+    turn_left()
+    turn_left()
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
