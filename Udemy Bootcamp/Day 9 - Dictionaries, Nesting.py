28562f9dd02f29bc5ac48352001087dd1e58b46e
Taekyu-Na <factualist623@gmail.com>
2026-01-06 20:57:08 +0900

    Update Day 9 - Dictionaries, Nesting.py

--- Udemy Bootcamp/Day 9 - Dictionaries, Nesting.py
+++ Udemy Bootcamp/Day 9 - Dictionaries, Nesting.py
@@ -23,3 +23,30 @@ for key in programming_dictionary:
     print(programming_dictionary[key])
 
 Nesting
+
+# TODO-1: Ask the user for input
+# TODO-2: Save data into dictionary {name: price}
+# TODO-3: Whether if new bids need to be added
+# TODO-4: Compare bids in dictionary
+
+dictionary = {
+    "name": [],
+    "price": []
+}
+
+more_bidders = True
+while more_bidders == True:
+    name = input("What is your name?   ")
+    bid = int(input("What is your bid?  : $ "))
+
+    dictionary["name"] += [name]
+    dictionary["price"] += [bid]
+
+    more_bid = input("Are there are any other bidders? Type 'yes' or 'no'.\n").lower()
+    if more_bid == "yes":
+        print("\n" * 100)
+    elif more_bid == "no":
+        max_bid = max(dictionary["price"])
+        bid_index = dictionary["price"].index(max_bid)
+        print(f"The winner is {dictionary["name"][bid_index]} with a bid of ${max_bid}")
+        more_bidders = False
