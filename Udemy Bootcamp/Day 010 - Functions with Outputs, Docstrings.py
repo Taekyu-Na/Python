4a69955f2b17ff110a7c00632e453774c9bfcdf8
Taekyu-Na <factualist623@gmail.com>
2026-01-15 22:42:51 +0900

    Update and rename Day 010 - Functions with Outputs.py to Day 010 - Functions with Outputs, Docstrings.py

--- Udemy Bootcamp/Day 010 - Functions with Outputs, Docstrings.py
+++ Udemy Bootcamp/Day 010 - Functions with Outputs, Docstrings.py
@@ -28,3 +28,13 @@ def function_2(text):
     return text.title()
 
 print(function_2(function_1("tk")))
+
+def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You didn't input any values."
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"{formated_f_name} {formated_l_name}"
+
+print(format_name(input("What is your first name?"), input("What is your last name?")))
+
