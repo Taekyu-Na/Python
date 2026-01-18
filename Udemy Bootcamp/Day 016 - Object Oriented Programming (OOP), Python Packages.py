6c0f6b9c98361fec91313f38f1df67623c6a4fe7
Taekyu-Na <factualist623@gmail.com>
2026-01-18 23:10:39 +0900

    Create Day 016 - Object Oriented Programming (OOP), Python Packages.py

---
+++ Udemy Bootcamp/Day 016 - Object Oriented Programming (OOP), Python Packages.py
@@ -0,0 +1,28 @@
+blueprint == class
+
+car = CarBlueprint()
+
+# Waiter
+has == attributes (data)
+is_holding_plate = True
+tables_responsible = [4, 5, 6]
+
+does == methods (function)
+def take_order(table, order):
+  # take order
+
+def take_payment(amount):
+  # add money
+
+  import turtle
+
+#object       #class
+timmy = turtle.Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("red")
+timmy.forward(100)
+
+my_screen = turtle.Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
