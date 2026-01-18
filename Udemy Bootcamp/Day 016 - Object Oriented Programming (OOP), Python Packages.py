ff5df3782afc8279073bb75e6287eae7fc77713b
Taekyu-Na <factualist623@gmail.com>
2026-01-19 00:28:21 +0900

    Update Day 016 - Object Oriented Programming (OOP), Python Packages.py

--- Udemy Bootcamp/Day 016 - Object Oriented Programming (OOP), Python Packages.py
+++ Udemy Bootcamp/Day 016 - Object Oriented Programming (OOP), Python Packages.py
@@ -26,3 +26,39 @@ timmy.forward(100)
 my_screen = turtle.Screen()
 print(my_screen.canvheight)
 my_screen.exitonclick()
+
+# PrettyTable
+
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Ticker name", ["TSLA", "NVDA", "GOOGL"])
+table.add_column("Description", ["Tesla", "NVIDIA", "Google"])
+table.align["Description"] = "l"
+
+print(table)
+
+Project - Coffee Machine
+
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}):  ").lower()
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        money_machine.report()
+        coffee_maker.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
