6dbb900d488588dfd8ef7a8eed40a3264970721d
Taekyu-Na <factualist623@gmail.com>
2026-01-24 14:05:09 +0900

    Update Day 025 - Working with CSV Files and Analyzing Data with Pandas.py

--- Udemy Bootcamp/Day 025 - Working with CSV Files and Analyzing Data with Pandas.py
+++ Udemy Bootcamp/Day 025 - Working with CSV Files and Analyzing Data with Pandas.py
@@ -84,3 +84,16 @@ for color in fur_color:
 
 data = pandas.DataFrame(dictionary)
 data.to_csv("squirrel_count.csv")
+
+# 선생님 답
+gray_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray_squirrels_count, red_squirrels_count, black_squirrels_count]
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count_answer")
