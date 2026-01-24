64f85f28a9441b633b98ff1593a4f70f81058f82
Taekyu-Na <factualist623@gmail.com>
2026-01-24 11:05:47 +0900

    Update Day 025 - Working with CSV Files and Analyzing Data with Pandas.py

--- Udemy Bootcamp/Day 025 - Working with CSV Files and Analyzing Data with Pandas.py
+++ Udemy Bootcamp/Day 025 - Working with CSV Files and Analyzing Data with Pandas.py
@@ -1,1 +1,20 @@
-jj
+with open("weather_data.csv") as file:
+    data = file.readlines()
+    print(data)
+
+import csv
+
+with open("weather_data.csv") as file:
+    data = csv.reader(file)
+    temperature = []
+    for row in data:
+        if row[1] != "temp":
+            temperature.append(int(row[1]))
+    print(temperature)
+
+# pandas 사용하기
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+# print(data)
+print(data["temp"])
