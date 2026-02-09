0007ea517e01ecd453abbda6d281ce43e3ebd5f3
Taekyu-Na <factualist623@gmail.com>
2026-02-09 22:51:24 +0900

    Update Day 26 - List and Dictionary Comprehensions.py

--- Udemy Bootcamp/Day 26 - List and Dictionary Comprehensions.py
+++ Udemy Bootcamp/Day 26 - List and Dictionary Comprehensions.py
@@ -19,7 +19,6 @@ new_list = [letter for letter in name]
 new_num = [n * 2 for n in range(1, 5)]
 
 Conditional List Comprehension
-
 new_list = [new_item for item in list if test]
 
 names = ["Alex", "Beth", "Caroline", "Dave", "Eugene", "Freddy"]
@@ -39,3 +38,64 @@ result = [num for num in numbers if num % 2 == 0]
 print(result)
 
 # 데이터 중첩
+with open("file1.txt") as file1:
+    list1 = file1.readlines()
+with open("file2.txt") as file2:
+    list2 = file2.readlines()
+
+result = [int(num) for num in list1 if num in list2]
+print(result)
+
+Dictionary Comprehension
+new_dict = {new_key:new_value for item in list}
+new_dict = {new_key:new_value for (key,value) in dict.items()}
+
+Conditional Dictionary Comprehension
+new_dict = {new_key:new_value for (key,value) in dict.items() if test}
+
+# 임의 점수 부여하기
+names = ["Alex", "Beth", "Caroline", "Dave", "Eugene", "Freddy"]
+import random
+students_scores = {student:random.randint(1, 100) for student in names}
+# 60점 이상인 학생만 추출하기
+passed_students = {student:score for (student,score) in students_scores.items() if score >= 60}
+
+# Dictionary Comprehension 실습 (1)
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+result = {word:len(word) for word in sentence.split()}
+
+# Dictionary Comprehension 실습 (2)
+weather_c = {"Monday": 12, "Tuesday": 14, "Wednesday": 15, "Thursday": 14, "Friday": 21, "Saturday": 22, "Sunday": 24}
+weather_f = {day:(cel * 9/5) + 32 for (day,cel) in weather_c.items()}
+print(weather_f)
+
+Looping through dictionary
+student_dict = {
+    "student" : ["TK", "Tboy"],
+    "score" : [48, 87]
+}
+
+for (key, value) in student_dict.items():
+    print(key)
+    print(value)
+
+Looping through dataframe
+import pandas
+
+student_df = pandas.DataFrame(student_dict)
+# print(student_df)
+
+for (key, value) in student_df.items():
+    # print(key)
+    print(value)
+
+Looping through rows of a dataframe
+for (index, row) in student_df.iterrows():
+    print(row.student) # row는 series
+  if row.student == "TK":
+    print(row.score)
+
+Project
+
+
+
